7d03d15412ee3593d246117b40bcb8745cf9f71f
Jacek Piętal <prozi85@gmail.com>
2016-11-30 23:39:52 +0100

    extend deps

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import App from './App';
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+const store = new Vuex.Store();
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: { App },
 });
